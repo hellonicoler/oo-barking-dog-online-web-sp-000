7b5217c32ca99e418b2be1044e05a9213ee5e954
Nicole Rothenay <nicoler@iccreativeagency.com>
2019-04-12 18:49:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Dog
+  def initialize(name, bark)
+    @name = name
+    @bark = bark
+  end
+  
+  #setter methods
+  def name=(name)
+    @name = name
+  end
+  def bark=(bark)
+    @bark = bark
+  end
+  
+  #getter methods
+  def name
+    @name
+  end
+  
+  def bark
+    @bark
+    puts "woof!"
+  end
+end
